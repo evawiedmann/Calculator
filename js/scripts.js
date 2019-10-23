f0968ff5cffa602cf465314a993d93e4cc887cfa
Student <example@example.com>
2019-10-23 14:30:08 -0700

    add UI for all functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -22,6 +24,8 @@ var exponent = function(number1, number2) {
   return number1 ** number2;
 };
 
+// User interface (or front-end) logic:
+
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
@@ -38,25 +42,35 @@ $(document).ready(function() {
     var result = subtract(number1, number2);
     $("#output2").text(result);
   });
-});
 
-    //
-    // var number1 = parseInt($("#multiply1").val());
-    // var number2 = parseInt($("#multiply2").val());
-    // var result = multiply(number1, number2);
-    // alert(result);
-    //
-    // var number1 = parseInt($("#divided1").val());
-    // var number2 = parseInt($("#divided2").val());
-    // var result = divided(number1, number2);
-    // alert(result);
-    //
-    // var number1 = parseInt($("#remainder1").val());
-    // var number2 = parseInt($("#remainder2").val());
-    // var result = remainder(number1, number2);
-    // alert(result);
-    //
-    // var number1 = parseInt($("#exponent1").val());
-    // var number2 = parseInt($("#exponent2").val());
-    // var result = exponent(number1, number2);
-    // alert(result);
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#output3").text(result);
+  });
+  $("form#divided").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divided1").val());
+    var number2 = parseInt($("#divided2").val());
+    var result = divided(number1, number2);
+    $("#output4").text(result);
+  });
+
+  $("form#remainder").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#remainder1").val());
+    var number2 = parseInt($("#remainder2").val());
+    var result = remainder(number1, number2);
+    $("#output5").text(result);
+  });
+  $("form#exponent").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#exponent1").val());
+    var number2 = parseInt($("#exponent2").val());
+    var result = exponent(number1, number2);
+    $("#output6").text(result);
+  });
+});
