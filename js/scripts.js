da43bb2dbcb7b9c56400a33e31035ced43a0fbac
Student <example@example.com>
2019-10-23 11:44:30 -0700

    test html v js responsiveness

--- js/scripts.js
+++ js/scripts.js
@@ -2,54 +2,55 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var remainder = function(number1, number2) {
+  return number1 % number2;
+};
+
+var divided = function(number1, number2) {
+  return number1 / number2;
+};
+
+var exponent = function(number1, number2) {
+  return number1 ** number2;
+};
+
+$(document).ready(function(){
+
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
 alert(result);
-
-
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = subtract(number1, number2);
 alert(result);
 
-
-var multiply = function(number1, number2) {
-  return number1 * number2;
-};
-
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = multiply(number1, number2);
 alert(result);
-
-var divided = function(number1, number2) {
-  return number1 / number2;
-};
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = divided(number1, number2);
 alert(result);
 
-var remainder = function(number1, number2) {
-  return number1 % number2;
-};
-
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = remainder(number1, number2);
 alert(result);
 
-var exponent = function(number1, number2) {
-  return number1 ** number2;
-};
-
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = exponent(number1, number2);
 alert(result);
+});
